84afccb3e2d5da8eaecc32df95b76e74abf27f66
Simonas Baltulionis <s.baltulionis@cid.com>
2022-03-23 13:58:54 +0200

    Enum0Literal example

--- source/DotNetExperiments/DotNetExperiments/CodeSamples/Enum0LiteralMethodOverload.cs
+++ source/DotNetExperiments/DotNetExperiments/CodeSamples/Enum0LiteralMethodOverload.cs
@@ -2,31 +2,29 @@
 
 namespace DotNetExperiments.CodeSamples
 {
-    public enum Color
-    {
-        Blue,
-        Red,
-        Yellow
-    }
+    public enum Color { Yellow, Green, Red }
 
     public static class Enum0MethodOverload
     {
-        static void Bar(Color f) => Console.WriteLine("Does X");
+        static void Bar(Color f) => Console.WriteLine("Enum");
 
         // After uncommenting this overload is chosen over the enum one
         static void Bar(int i) => Console.WriteLine("Does Y");
 
         public static void Invoke()
         {
-            Bar(Color.Blue);
+            int a = 0;
+            Bar(Color.Yellow);
             Bar(0);
+            Bar(1);
+            Bar(a);
         }
     }
 
     public static class Enum0MethodOverloadConstObject
     {
-        static void Bar(object o) => Console.WriteLine("Does X");
-        static void Bar(Color f) => Console.WriteLine("Does Y");
+        static void Bar(object o) => Console.WriteLine("Object");
+        static void Bar(Color f) => Console.WriteLine("Enum");
 
         public static void Invoke()
         {
